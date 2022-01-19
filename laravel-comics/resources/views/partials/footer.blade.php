be925f19426210afb0902f9984011c40a61233c3
“Cal-107” <kaly___7@hotmail.it>
2022-01-19 19:43:02 +0100

    add footer

--- laravel-comics/resources/views/partials/footer.blade.php
+++ laravel-comics/resources/views/partials/footer.blade.php
@@ -1,36 +1,38 @@
-<footer>
-    <button>
-        SIGN-UP NOW!
-    </button>
-
-    <div>
-        <span>FOLLOW US</span>
-        <ul>
-            <li>
-                <a href="#">
-                    <img src="{{ ('images/footer-facebook.png') }}" alt="">
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="{{ ('images/footer-twitter.png') }}" alt="">
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="{{ ('images/footer-youtube.png') }}" alt="">
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="{{ ('images/footer-pinterest.png') }}" alt="">
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="{{ ('images/footer-periscope.png') }}" alt="">
-                </a>
-            </li>
-        </ul>
-    </div>
+<footer class="main-footer">
+    <section class="container">
+        <button>
+            SIGN-UP NOW!
+        </button>
+    
+        <div class="list-box">
+            <span>FOLLOW US</span>
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="{{ ('images/footer-facebook.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ ('images/footer-twitter.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ ('images/footer-youtube.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ ('images/footer-pinterest.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ ('images/footer-periscope.png') }}" alt="">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
 </footer>
